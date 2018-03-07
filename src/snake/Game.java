75d6a84e39f336b732a97e32dcfc2a4404ff453e
JuanCarlosTapia <jc_tapia95@hotmail.com>
2018-03-06 18:40:04 -0600

    Multiples jugadores

--- src/snake/Game.java
+++ src/snake/Game.java
@@ -2,11 +2,14 @@ package snake;
 // Clase Game.java : Instancia la clase Game, el JFrame
 // Jose Fernando Flores
 import java.awt.EventQueue;
+import java.io.IOException;
+import java.net.UnknownHostException;
+
 import javax.swing.JFrame;
 
 public class Game extends JFrame {
 
-    public Game() {
+    public Game() throws UnknownHostException, IOException {
         add(new Board());
         setResizable(false);
         pack();
@@ -19,8 +22,18 @@ public class Game extends JFrame {
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
-                JFrame frame = new Game();
-                frame.setVisible(true);
+                JFrame frame;
+				try {
+					frame = new Game();
+					frame.setVisible(true);
+				} catch (UnknownHostException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+                
             }
         });
     }
